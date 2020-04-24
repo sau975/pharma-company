814387882448d5323b6b5c59c33e6fd14171fe4b
Shrikant Kumar <Saurav Kumar>
2020-04-24 22:18:35 +0530

    App Module

--- pharma-company-app/src/app/app.module.ts
+++ pharma-company-app/src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './customer-bc/services/in-memory-data.service';
+import { CustomerBcModule } from './customer-bc/customer-bc.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+    InMemoryDataService, { dataEncapsulation: false }),
+    CustomerBcModule
   ],
   providers: [],
   bootstrap: [AppComponent]
